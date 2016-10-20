ca012304403a76afbe32305bb94f649ad6590f6d
Andrei Ioan Popescu <andrei.ioan.popescu@ericsson.com>
2016-10-20 17:16:40 +0300

    added education

--- js/controller/EducationController.js
+++ js/controller/EducationController.js
@@ -1,13 +1,15 @@
-$scope.years = [{
-    "year": "2018",
-    "title": "Master of Business Administration And Engineering - UPB",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
-}, {
-    "year": "2016",
-    "title": "Computer Science (English) - UPB",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
-}, {
-    "year": "2012",
-    "title": "Mathematics/Informatics - Dinicu Golescu National College",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
-}];
+cvApp.controller('EducationController', ['$scope', function EducationController($scope) {
+  $scope.years = [{
+      "year": "2018",
+      "title": "Master of Business Administration And Engineering - UPB",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
+  }, {
+      "year": "2016",
+      "title": "Computer Science (English) - UPB",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
+  }, {
+      "year": "2012",
+      "title": "Mathematics/Informatics - Dinicu Golescu National College",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
+  }];
+}]);
