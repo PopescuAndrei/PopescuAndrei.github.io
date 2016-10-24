96590104fd8a43f7e85f7fa1874e35cad2b01c16
AndreiPopescu <andrei.popescu93@gmail.com>
2016-10-25 01:15:37 +0300

    Updated contacts section

--- js/controller/ContactsController.js
+++ js/controller/ContactsController.js
@@ -1,11 +1,24 @@
 cvApp.controller('ContactsController', ['$scope', function ContactsController($scope) {
-    $scope.contacts = [{
+    $scope.contacts = [
+    {
+        "title":"+40 745 314 873",
+        "icon":"fa fa-phone fa-fw",
+        "link":"tel:+40745314873"
+    }, {
+        "title":"Email me!",
+        "icon":"fa fa-envelope-o fa-fw",
+        "link":"mailto:andrei.popescu93@gmail.com"
+    }, {
         "title":"Github",
-        "icon":"fa fa-github fa-gh",
+        "icon":"fa fa-github fa-fw",
         "link":"https://www.github.com/PopescuAndrei"
     }, {
         "title":"Facebook",
         "icon":"fa fa-facebook fa-fw",
         "link":"https://www.facebook.com/andreiipopescu"
+    }, {
+        "title":"Linkedin",
+        "icon":"fa fa-linkedin fa-fw",
+        "link":"https://www.linkedin.com/in/andreiipopescu"
     }];
 }]);
--- js/controller/EducationController.js
+++ js/controller/EducationController.js
@@ -2,7 +2,7 @@ cvApp.controller('EducationController', ['$scope', function EducationController(
   $scope.years = [{
       "year": "2018",
       "title": "Master of Business Administration And Engineering - UPB",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, <strong>totam, dolores iste dolore est aut modi</strong>.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
   }, {
       "year": "2016",
       "title": "Computer Science (English) - UPB",
